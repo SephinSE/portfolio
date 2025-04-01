87ecffbdf602aa223e0f6721e58b075d3caa5a7f
Sephin Sebastian E <sephinsebastian0830@gmail.com>
2025-04-02 02:52:34 +0530

    overhaui 0.2

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"82bd5b7209295a5b7ff8cae0df96e7870171e3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2737253520"
+    serviceWorkerVersion: "1879043591"
   }
 });
